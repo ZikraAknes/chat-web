d7b765a6b24513282c2780a83eda140e397e1ded
ZikraAknes <zikra.taqi@gmail.com>
2025-06-03 21:27:11 +0700

    feat: whitenoise

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip3 install -r requirements.txt
 
+# Make sure static files are properly collected
 python3 manage.py collectstatic --noinput
 
 python3 manage.py makemigrations
--- chat_web/settings.py
+++ chat_web/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,6 +142,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Enable WhiteNoise compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
