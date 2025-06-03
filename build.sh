611bdf46afe34193a709730a3ebc910de9a6db61
ZikraAknes <zikra.taqi@gmail.com>
2025-06-03 21:18:23 +0700

    fix: build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ set -o errexit
 pip3 install -r requirements.txt
 
 python3 manage.py collectstatic --noinput
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+python3 manage.py populate_data
